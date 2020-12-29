af55373ca2ace895ee6d8c6fbe5f58b2f8528597
jajimer <javi.j21@gmail.com>
2020-12-29 14:04:09 +0100

    environments files created

--- gym_trader/__init__.py
+++ gym_trader/__init__.py
@@ -2,9 +2,9 @@ from gym.envs.registration import register
 
 register(
     id='single-discrete-v0',
-    entry_point='gym_foo.envs:SingleDiscreteEnv',
+    entry_point='gym_trader.envs:SingleDiscreteEnv',
 )
 register(
     id='single-continuous-v0',
-    entry_point='gym_foo.envs:SingleContinuousEnv',
+    entry_point='gym_trader.envs:SingleContinuousEnv',
 )
