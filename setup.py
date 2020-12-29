49ab9c47b32f155384264c91803a1ada3e6b54df
Javier Jiménez <35655092+jajimer@users.noreply.github.com>
2020-12-29 13:43:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup
 
+setup(
+    name="gym_trader",
+    version="0.0.1",
+    author="Javier Jiménez-Raboso",
+    author_email="javi.j21@gmail.com",
+    description="An OpenAI Gym environment for trading",
+    install_requires=['gym', 'pandas>=1.0']
+)
