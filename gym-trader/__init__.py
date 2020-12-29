7d043cbd8988a139f0b94149e1ad319e58946daf
Javier Jiménez <35655092+jajimer@users.noreply.github.com>
2020-12-29 13:49:41 +0100

    Update __init__.py

--- gym-trader/__init__.py
+++ gym-trader/__init__.py
@@ -1,1 +1,10 @@
+from gym.envs.registration import register
 
+register(
+    id='single-discrete-v0',
+    entry_point='gym_foo.envs:SingleDiscreteEnv',
+)
+register(
+    id='single-continuous-v0',
+    entry_point='gym_foo.envs:SingleContinuousEnv',
+)
